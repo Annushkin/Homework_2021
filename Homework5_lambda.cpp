de61a7b13b70ed7490325f7a32846f7ed5e736b1
Annushkin <55763668+Annushkin@users.noreply.github.com>
2021-11-23 16:23:51 +0300

    Update Homework5_lambda.cpp

--- Homework5_lambda.cpp
+++ Homework5_lambda.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include<vector>
 #include<deque>
+#include<list>
 
 template<typename T>
 T Palindrom(T& words)
@@ -14,24 +15,24 @@ T Palindrom(T& words)
         return true;              	
     }
 };
-   T result;
-   for(int i = 0; i < words.size(); ++i){
-   	  if(p(words[i]))
-   	  	 result.push_back(words[i]); 	
+      T result;
+   for(auto it = words.begin(); it != words.end(); ++it){
+   	  if(p(*it))
+   	  	 result.push_back(*it); 	
    }  
     return result;
 }
 
-
 int main(){
 	  
-	//std::vector<std::string>words = {"lol", "topot", "the", "cooc", "this"};  
-	std::deque<std::string>words = {"lol", "topot", "the", "cooc", "these"};
+//std::vector<std::string>words = {"lol", "topot", "the", "cooc", "these"};  
+//std::list<std::string>words = {"lol", "topot", "the", "cooc", "these"};
+std::deque<std::string>words = {"lol", "topot", "the", "cooc", "these"};	
 	
+//std::vector<std::string>res = Palindrom(words);
+//std::list<std::string>res = Palindrom(words);
+std::deque<std::string>res = Palindrom(words);
 	
-	//std::vector<std::string>res = Palindrom(words);
-    std::deque<std::string>res = Palindrom(words);
-	
-	for(auto &i: res) std::cout << i << " ";   
+for(auto &i: res) std::cout << i << " ";   
 
 }
