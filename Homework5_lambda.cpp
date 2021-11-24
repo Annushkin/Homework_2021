2b7a4e9a6bc851310763ea7c4b305096cbc97513
Annushkin <55763668+Annushkin@users.noreply.github.com>
2021-11-24 23:31:23 +0300

    Update Homework5_lambda.cpp

--- Homework5_lambda.cpp
+++ Homework5_lambda.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 #include<string>
 #include<vector>
-#include<deque>
 #include<list>
+#include<deque>
+#include<set>
 
 template<typename T>
 T Palindrom(T& words)
 {	
-	auto p = [](std::string & words) -> bool{	
+	auto p = [](std::string words) -> bool{	
     if (words.size() < 2) return true;
     else{	
 		for (int i = 0; i < words.size()/2; ++i)
@@ -17,25 +18,40 @@ T Palindrom(T& words)
 	        return true;              	
 	    }
  	}
-};
-      T result;
-   for(auto it = words.begin(); it != words.end(); ++it){
-   	  if(p(*it))
-   	  	 result.push_back(*it); 	
-   }  
+};  
+   
+   T result; 	
+   	for(auto &i: words)  
+   		if (p(i)) result.insert(result.end(), i);	
+   		
     return result;
 }
 
-int main(){
-	  
-//std::vector<std::string>words = {"lol", "topot", "the", "cooc", "these", " ", "l"};  
-//std::list<std::string>words = {"lol", "topot", "the", "cooc", "these", " ", "l"};
-std::deque<std::string>words = {"lol", "topot", "the", "cooc", "these", " ", "l"};	
-	
-//std::vector<std::string>res = Palindrom(words);
-//std::list<std::string>res = Palindrom(words);
-std::deque<std::string>res = Palindrom(words);
-	
-for(auto &i: res) std::cout << i << " ";   
+int main(){	  
 
+	std::vector<std::string>words_v = {"lol", "topot", "the", "cooc", "these", " ", "l"};
+	std::list<std::string>words_l = {"lol", "topot", "the", "cooc", "these", " ", "l"};
+	std::deque<std::string>words_d = {"lol", "topot", "the", "cooc", "these", " ", "l"};
+	std::set<std::string>words_s = {"lol", "topot", "the", "cooc", "these", " ", "l"};	
+
+	std::vector<std::string>res_v = Palindrom(words_v);
+	std::list<std::string>res_l = Palindrom(words_l);
+	std::deque<std::string>res_d = Palindrom(words_d);
+	std::set<std::string>res_s = Palindrom(words_s);
+	
+	std::cout << "v: ";
+	for(auto &i: res_v) std::cout << i << " ";  
+	std::cout << '\n' << '\n'; 
+	
+	std::cout << "l: ";
+	for(auto &i: res_l) std::cout << i << " ";  
+	std::cout << '\n' << '\n';
+	
+	std::cout << "d: ";
+	for(auto &i: res_d) std::cout << i << " ";  
+	std::cout << '\n' << '\n';
+	
+	std::cout << "s: ";
+	for(auto &i: res_s) std::cout << i << " ";  
+	std::cout << '\n' << '\n';
 }
